f8ad2dbfe902bb091e8a998424dc00d1e081ee80
Fran <francinele.coliveira@gmail.com>
2022-03-28 19:10:27 -0300

    cria radio buttons para a SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function SearchBar() {
+  return (
+    <div>
+      <input
+        data-testid="search-input"
+        type="text"
+        name="search-input"
+      />
+      <input
+        data-testid="ingredient-search-radio"
+        type="radio"
+        name="ingredient-search"
+      />
+      Ingredient
+      <input
+        data-testid="name-search-radio"
+        type="radio"
+        name="name-search"
+      />
+      Name
+      <input
+        data-testid="first-letter-search-radio"
+        type="radio"
+        name="first-letter-search"
+      />
+      First Letter
+      <button
+        data-testid="exec-search-btn"
+        type="button"
+        name="search-button"
+      >
+        Search
+      </button>
+    </div>
+  );
+}
