1429a1a04e108f75eba3aba70ea06d89b83afef7
Aline <allinne.oliveira.ol@gmail.com>
2022-03-28 20:02:41 -0300

    testes da tela de login com 80% de cobertura

--- src/testes/login.test.js
+++ src/testes/login.test.js
@@ -1,7 +1,44 @@
-// import React from 'react';
-// import { cleanup, fireEvent, render, screen } from '@testing-library/react';
-// import Login from '../pages/Login';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Login from '../pages/Login';
+import renderWithRouter from './renderWithRouter';
 
-// describe('Os devem respeitar os atributos descritos no protótipo para a tela de login', () => {
-//   it('o imput de email deve ter o atributo data-testid');
-// });
+describe('Verifica validações na tela de login', () => {
+  it('testa se ao renderizar a tela o botão de login esta desativado', () => {
+    renderWithRouter(<Login />);
+    const button = screen.getByRole('button');
+    expect(button).toBeDisabled();
+  });
+
+  it('Ao digitar o email e a senha corretamente o botão de login fica desativado', () => {
+    renderWithRouter(<Login />);
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    userEvent.type(inputEmail, 'usuario@123.com');
+    userEvent.type(inputPassword, '1234567');
+    const button = screen.getByRole('button');
+    expect(button).not.toBeDisabled();
+  });
+  it('Ao digitar email  senha incorretamente, o botão de login fica desativado', () => {
+    renderWithRouter(<Login />);
+    const inputEmail = screen.getByTestId('email-input');
+    const inputPassword = screen.getByTestId('password-input');
+    userEvent.type(inputEmail, 'usuario@123');
+    userEvent.type(inputPassword, '1234567');
+    const button = screen.getByRole('button');
+    expect(button).toBeDisabled();
+    userEvent.type(inputEmail, 'usuario123.com');
+    userEvent.type(inputPassword, '1234567');
+    expect(button).toBeDisabled();
+    userEvent.type(inputEmail, '@123.com');
+    userEvent.type(inputPassword, '1234567');
+    expect(button).toBeDisabled();
+    userEvent.type(inputEmail, 'usuario@123.com');
+    userEvent.type(inputPassword, '123456');
+    expect(button).toBeDisabled();
+  });
+});
+describe('Verifica se renderiza a tela de principal de receitas ao fazer login', () => {
+
+});
