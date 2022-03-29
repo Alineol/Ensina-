195f3d2f3679f8bcd8eaf36843c273f16159fce7
Fran <francinele.coliveira@gmail.com>
2022-03-29 17:14:40 -0300

    Inicia o componente de testes da SearchBar

---
+++ src/testes/searchBar.test.js
@@ -0,0 +1,31 @@
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import testData from '../testData';
+
+const mockFetch = () => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementation(() => Promise.resolve({
+      status: 200,
+      ok: true,
+      json: () => Promise.resolve(testData),
+    }));
+};
+
+describe('Testa a Barra de Pesquisa', () => {
+  beforeAll(mockFetch);
+  it('Realize uma requisição para a API', async () => {
+    await act(async () => {
+      render(<App />);
+    });
+    expect(global.fetch).toHaveBeenCalled();
+  });
+
+  it('verifica se os radio buttons de filtro estão presentes na tela', async () => {
+    await act(async () => {
+      render(<App />);
+    });
+    const ingredientFilter = screen.getByTestId('search-input');
+    expect(ingredientFilter).toBeInTheDocument();
+  });
+});
