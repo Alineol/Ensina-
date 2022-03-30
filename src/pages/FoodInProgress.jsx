186d174c21110153435181b280e0891b250198d5
Aline Salema <alineelainesalema@gmail.com>
2022-03-30 16:47:29 -0300

    Tela de receita em progresso

--- src/pages/FoodInProgress.jsx
+++ src/pages/FoodInProgress.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 function FoodInProgress() {
   return (
-    <h1>Food in progreess page</h1>
+    <div>
+      <h1>Food in progreess page</h1>
+      <img data-testid="recipe-photo" src="img" alt="RecipePhoto" />
+      <h2 data-testid="recipe-title"> Nome da Receita</h2>
+      <button type="button" data-testid="share-btn">Compartilhar</button>
+      <button type="button" data-testid="favorite-btn">Favorito</button>
+      <p data-testid="recipe-category">texto da categoria</p>
+      <h3>Ingredients</h3>
+      <li>Lista ingredientes</li>
+      <h3>Instructions</h3>
+      <p data-testid="instructions">Texto instruções</p>
+      <button type="button" data-testid="finish-recipe-btn">Finish Recipe</button>
+
+    </div>
+
   );
 }
 
