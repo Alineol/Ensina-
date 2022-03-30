f5ead0b0f99b3d149f4c6352e12a655c20e71c6e
Aline <allinne.oliveira.ol@gmail.com>
2022-03-30 18:15:06 -0300

    requisitos ainda não finalizados

--- src/pages/ExploreFoodsByIngredient.jsx
+++ src/pages/ExploreFoodsByIngredient.jsx
@@ -3,6 +3,19 @@ import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
 function ExploreFoodsByIngredient() {
+  const getIngredients = async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const maxIndex = 12;
+    const data = await response.json();
+    const twelveIngredients = data.meals.slice(0, maxIndex);
+    return twelveIngredients;
+  };
+  const getIngredienteImg = async (ingredient) => {
+    const response = await fetch(`https://www.themealdb.com/images/ingredients/${ingredient}-Small.png`);
+    return (response.url);
+  };
+  getIngredients();
+  getIngredienteImg('banana');
   return (
     <>
       <Header pageTitle="Explore Ingredients" showSearchButton={ false } />
