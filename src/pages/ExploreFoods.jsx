daa634472b9c5a96a87a26140b154342b5fcb50f
Aline <allinne.oliveira.ol@gmail.com>
2022-03-30 17:33:40 -0300

    adiciona tela de opções de exploração

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MenuInferior from '../components/MenuInferior';
 
-function ExploreFoods() {
+function ExploreFoods(props) {
+  const getRandomFood = async () => {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+    return (data.meals[0].idMeal);
+  };
+
+  const { history } = props;
   return (
     <>
       <Header pageTitle="Explore Foods" showSearchButton={ false } />
+      <div className="explore-div">
+        <button
+          onClick={ () => history.push('/explore/foods/ingredients') }
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+        <button
+          onClick={ () => history.push('/explore/foods/nationalities') }
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+        <button
+          onClick={ async () => history.push(`/foods/${await getRandomFood()}`) }
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </div>
       <MenuInferior />
     </>
   );
 }
+ExploreFoods.propTypes = {
+  history: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
 export default ExploreFoods;
