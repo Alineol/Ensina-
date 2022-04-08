2737d9432944f930ebca72060e17ed8135582ae3
Aline Salema <alineelainesalema@gmail.com>
2022-04-07 21:17:56 -0300

    Tela de Receitas favoritas finalizada

--- src/components/CardFavorite.jsx
+++ src/components/CardFavorite.jsx
@@ -18,80 +18,85 @@ function CardFavorite({ filter }) {
     if (favoriteRecipes && filter === 'food') {
       const foodsList = favoriteRecipes
         .filter((savedRecipes) => savedRecipes.type === 'food');
+
       setFilteredRecipes(foodsList);
     }
     if (favoriteRecipes && filter === 'drink') {
       const drinkList = favoriteRecipes
         .filter((savedRecipes) => savedRecipes.type === 'drink');
+
       setFilteredRecipes(drinkList);
     }
   }, [filter]);
 
-  const handleClickCopyLinkButton = (e) => {
+  const handleClickCopyLinkButton = (e, recipe) => {
     if (recipe.type === 'food') {
       const linkCopied = 'Link copied!';
       global.alert(linkCopied);
       const url = `http://localhost:3000/foods/${recipe.id}`;
       copyToClipboard(url);
       e.target.innerText = linkCopied;
-      console.log(id);
     } else {
       const linkCopied = 'Link copied!';
       global.alert(linkCopied);
       const url = `http://localhost:3000/drinks/${recipe.id}`;
       copyToClipboard(url);
       e.target.innerText = linkCopied;
-      console.log(id);
     }
   };
 
-  const handleClickOnFavoriteButton = () => {
+  const handleClickOnFavoriteButton = (recipe) => {
     const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    console.log(favoriteRecipes);
-    const newFavoriteRecipes = favoriteRecipes.filter((recipe) => recipe.id !== id);
-    console.log(newFavoriteRecipes);
+    const newFavoriteRecipes = favoriteRecipes.filter((e) => e.id !== recipe.id);
+
     localStorage.setItem('favoriteRecipes', JSON.stringify(newFavoriteRecipes));
+    setFilteredRecipes(newFavoriteRecipes);
     setShowFavoriteRecipe(!showFavoriteRecipe);
   };
 
-  const createCard = () => filteredRecipes.map((recipe, index) => (
-    <section key={ recipe.id }>
-      <Link to={ `/foods/${recipe.id}` }>
-        <img
-          src={ recipe.image }
-          alt=""
-          data-testid={ `${index}-horizontal-image` }
-          width="200px"
-        />
-      </Link>
-      <Link to={ `/foods/${recipe.id}` }>
-        <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-      </Link>
-      <p
-        data-testid={ `${recipe.index}-horizontal-top-text` }
-      >
-        { `${recipe.nationality} - ${recipe.category}` }
-      </p>
-      <button
-        type="button"
-        data-testid={ `${index}-horizontal-share-btn` }
-        src={ shareIcon }
-        onClick={ handleClickCopyLinkButton }
-      >
-        Compartilhar
-      </button>
-      <button
-        type="button"
-        name={ `${index}-horizontal-favorite-btn` }
-        data-testid={ `${index}-horizontal-favorite-btn` }
-        className="favorite-btn"
-        src={ blackHeart }
-        onClick={ handleClickOnFavoriteButton }
-      >
-        <img src={ blackHeart } alt="" />
-      </button>
-    </section>
-  ));
+  const createCard = () => filteredRecipes.map((recipe, index) => {
+    const { type } = recipe;
+    return (
+      <section key={ recipe.id }>
+        <Link to={ `/${type}s/${recipe.id}` }>
+          <img
+            src={ recipe.image }
+            alt=""
+            data-testid={ `${index}-horizontal-image` }
+            width="200px"
+          />
+        </Link>
+        <Link to={ `/${type}s/${recipe.id}` }>
+          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+        </Link>
+        <p
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          {
+            recipe.alcoholicOrNot
+              || `${recipe.nationality} - ${recipe.category}`
+          }
+        </p>
+        <button
+          type="button"
+          data-testid={ `${index}-horizontal-share-btn` }
+          src={ shareIcon }
+          onClick={ (e) => handleClickCopyLinkButton(e, recipe) }
+        >
+          Compartilhar
+        </button>
+        <button
+          type="button"
+          name={ `${index}-horizontal-favorite-btn` }
+          data-testid={ `${index}-horizontal-favorite-btn` }
+          className="favorite-btn"
+          src={ blackHeart }
+          onClick={ () => handleClickOnFavoriteButton(recipe) }
+        >
+          <img src={ blackHeart } alt="" />
+        </button>
+      </section>);
+  });
   return (
     <section>
       {filteredRecipes[0] ? createCard()
@@ -101,8 +106,17 @@ function CardFavorite({ filter }) {
 }
 
 CardFavorite.propTypes = {
+//   id: PropTypes.string.isRequired,
+//   name: PropTypes.string.isRequired,
+//   image: PropTypes.string.isRequired,
+//   category: PropTypes.string.isRequired,
+//   nationality: PropTypes.string.isRequired,
+//   type: PropTypes.string.isRequired,
+//   alcoholicOrNot: PropTypes.string.isRequired,
   filter: PropTypes.string.isRequired,
 };
+
+export default CardFavorite;
 
 // const renderFood = filteredRecipes.map((recipe, index) => (
 
@@ -161,5 +175,3 @@ CardFavorite.propTypes = {
 //   alcoholicOrNot: PropTypes.string.isRequired,
 //   filter: PropTypes.string.isRequired,
 // };
-
-export default CardFavorite;
