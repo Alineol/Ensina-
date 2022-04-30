14578d0586701a8f9cfd1b48ef8959074ca1848f
Aline <allinne.oliveira.ol@gmail.com>
2022-04-30 16:44:00 -0300

    remove useState duplicado e altera css da label

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,47 +1,37 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useParams } from 'react-router-dom';
 import context from '../context/context';
 import { checkCheked } from '../services/helpers';
 
-function Ingredient({ name, selectable, isSelected, dataTestId }) {
+function Ingredient({ name, selectable, dataTestId }) {
   const { location: { pathname } } = useHistory();
   const { id } = useParams();
   const { setIngredientChecked, ingredientClick,
     setIngredientClick } = useContext(context);
-  const [selected, setSelected] = useState(false);
   const [check, setCheck] = useState(checkCheked(pathname, name, id));
 
-  useEffect(() => {
-    setSelected(isSelected);
-    if (check) {
-      setSelected(true);
-    }
-  }, [isSelected]);
-
   const handleOnChangeInput = (ingredient) => {
-    setSelected(!selected);
     setCheck(!check);
     setIngredientChecked(ingredient);
   };
 
   const renderSelectableIngredient = () => (
     <li data-testid={ dataTestId }>
+      <input
+        name={ `${name}-ingredient` }
+        id={ `${name}-ingredient` }
+        type="checkbox"
+        value={ name }
+        className="ingredienteProgress"
+        onChange={ (e) => handleOnChangeInput(e.target.value) }
+        checked={ check }
+        onClick={ () => setIngredientClick(!ingredientClick) }
+      />
       <label
         htmlFor={ `${name}-ingredient` }
-        style={ { textDecoration: selected ? 'line-through' : null } }
       >
-        <input
-          name={ `${name}-ingredient` }
-          id={ `${name}-ingredient` }
-          type="checkbox"
-          value={ name }
-          className="ingredienteProgress"
-          onChange={ (e) => handleOnChangeInput(e.target.value) }
-          checked={ check }
-          onClick={ () => setIngredientClick(!ingredientClick) }
-        />
         { name}
       </label>
     </li>
@@ -61,7 +51,6 @@ function Ingredient({ name, selectable, isSelected, dataTestId }) {
 Ingredient.propTypes = {
   name: PropTypes.string.isRequired,
   selectable: PropTypes.bool.isRequired,
-  isSelected: PropTypes.bool.isRequired,
   dataTestId: PropTypes.string.isRequired,
 };
 
